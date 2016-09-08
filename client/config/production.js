3baa67f186212c2883ce53143eaa2035b62ed6b5
name <salonparu@gmail.com>
2016-09-08 10:16:21 +0000

    change production.js

--- client/config/production.js
+++ client/config/production.js
@@ -19,4 +19,11 @@ module.exports = {
       },
     ]
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": { 
+         NODE_ENV: JSON.stringify("production") 
+       }
+    })
+  ]
 }
