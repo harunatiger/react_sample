ef96cdddd81200c580722f494fc0eb298196639a
name <salonparu@gmail.com>
2016-09-08 10:18:00 +0000

    a

--- client/config/production.js
+++ client/config/production.js
@@ -17,13 +17,13 @@ module.exports = {
           presets: ["es2015", "react"],
         }
       },
+    ],
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env": { 
+           NODE_ENV: JSON.stringify("production") 
+         }
+      })
     ]
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env": { 
-         NODE_ENV: JSON.stringify("production") 
-       }
-    })
-  ]
 }
